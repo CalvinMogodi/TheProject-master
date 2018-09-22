92922cb46d35cf16d1afff58e67b65b5cad1d04b
Calvin <cmogodi@gmail.com>
2018-09-22 02:00:38 +0200

    Add project files.

---
+++ TheProject.Model/Location.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TheProject.Model
+{
+    public class Location
+    {
+        public int Id { get; set; }
+
+        public string StreetAddress { get; set; }
+
+        public string Suburb { get; set; }
+
+        public string Province { get; set; }
+
+        public string LocalMunicipality { get; set; }
+
+        public string Region { get; set; }
+
+        public List<BoundryPolygon> BoundryPolygon { get; set; }
+
+        public GPSCoordinate GPSCoordinates { get; set; }
+
+        //public DateTime CreatedDate { get; set; }
+
+        //public DateTime? ModifiedDate { get; set; }
+
+        //public int CreatedUserId { get; set; }
+
+        //public int? ModifiedUserId { get; set; }
+    }
+}
